4b19f0e99c7000c46b32c4f80fb1397c197d7cdd
Gh0stcrab <60898498+Gh0strab@users.noreply.github.com>
2020-02-11 10:24:42 -0600

    Update and rename ZeroGyroCommand.java.txt to ZeroGyroCommand.java

--- src/main/java/frc/robot/commands/ZeroGyroCommand.java
+++ src/main/java/frc/robot/commands/ZeroGyroCommand.java
@@ -1,12 +1,12 @@
-
+//idk if this works
 package org.team2471.frc.steamworks.commands;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
-import org.team2471.frc.steamworks.HardwareMap;
 
 public class ZeroGyroCommand extends InstantCommand {
+public static final Gyro gyro = new Gyro(new NavX());
   @Override
   protected void initialize() {
-    HardwareMap.gyro.reset();
+    gyro.reset();
   }
 }
