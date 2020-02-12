af8f9c447f81da125c124711539b9ac7a61452aa
Gh0stcrab <60898498+Gh0strab@users.noreply.github.com>
2020-02-12 11:33:41 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/AutonomousRoutines/AMidRun12Point.java
@@ -0,0 +1,7 @@
+package frc.robot.AutonomousRoutines;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.Encoder;
+public class AMidRun12Point extends CommandGroup{
+
+}
---
+++ src/main/java/frc/robot/AutonomousRoutines/BMidRun12Point.java
@@ -0,0 +1,7 @@
+package frc.robot.AutonomousRoutines;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class BMidRun12Point extends CommandGroup{
+
+}
