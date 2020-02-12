7a0eb21a23999edc890cb3939618304549f3d41b
Gh0stcrab <60898498+Gh0strab@users.noreply.github.com>
2020-02-12 10:31:14 -0600

    Add files via upload

--- src/main/java/frc/robot/commands/ZeroGyroCommand.java
+++ src/main/java/frc/robot/commands/ZeroGyroCommand.java
@@ -2,14 +2,14 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
-import edu.wpi.first.wpilibj.I2C;
-//import com.kauailabs.navx.frc.AHRS;
-
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
 public class ZeroGyroCommand extends InstantCommand {
-  //ahrs = new AHRS(I2C.kMXP);
-public static final Gyro gyro = new Gyro(new ahrs());
+ 
+public static final AHRS ahrs = new AHRS(SPI.Port.kMXP);
   @Override
   protected void initialize() {
-    gyro.reset();
+    ahrs.reset();
+    
   }
 }
